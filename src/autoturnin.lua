208abff552ca1958c5ba12d5c9e89f0b41407aae
Giovanni Cavallo <giovannicavallo95@gmail.com>
2022-11-29 15:05:55 +0100

    init experimenta autoturnin integration

--- src/autoturnin.lua
+++ src/autoturnin.lua
@@ -2,26 +2,43 @@ local __namespace, __module = ...
 
 local Addon = __module.Addon --- @class Addon
 
+local onLoad = Addon.onLoad
 local useHook = Addon.useHook
+local useEvent = Addon.useEvent
 
 local module = {}
 
--- useHook(
---   "QUEST_DETAIL", function(self)
---     C_Timer.After(2, self.__oldFnSelf)
---   end, "function", AutoTurnIn
--- )
+local hook = false
+local nop = function()
+end
+local nextAction = nop
 
--- local _QUEST_DETAIL = AutoTurnIn.QUEST_DETAIL
--- AutoTurnIn.QUEST_DETAIL = function(...)
---   local args = { ... }
+function module.getFrame()
+  return AutoTurnIn
+end
 
---   useEvent(
---     function()
---       _QUEST_DETAIL(unpack(args))
---     end,
---     { "VOICE_CHAT_TTS_PLAYBACK_FINISHED", "VOICE_CHAT_TTS_PLAYBACK_FAILED" },
---     true
---   )
--- end
+function module.continue()
+  nextAction()
+  nextAction = nop
+end
 
+onLoad(
+  function()
+    if not module.getFrame() or not hook then
+      return
+    end
+
+    useHook(
+      "QUEST_DETAIL", function(self, ...)
+        local args = { ... }
+
+        nextAction = function()
+          self.__oldFn(self.__srcTable, unpack(args))
+        end
+      end, "function", AutoTurnIn
+    )
+
+  end
+)
+
+__module.AutoTurnIn = module
